1782a08b947777565915b7579f01250c3261fc87
isabellyccamargo <camargoisa114@gmail.com>
2026-06-02 11:38:48 -0300

    chore: ajustes para uso do Docker-compose

--- UrbanCandy_back/src/config/Config.ts
+++ UrbanCandy_back/src/config/Config.ts
@@ -1,12 +1,12 @@
 import { Sequelize } from 'sequelize';
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME || 'urbancandy', 
-  process.env.DB_USER || 'root', 
-  process.env.DB_PASSWORD || 'ADMIN', 
+  process.env.DB_NAME || 'urbancandy',
+  process.env.DB_USER || 'root',
+  process.env.DB_PASSWORD || 'ADMIN',
   {
     // Se estiver no Docker, ele usa o DB_HOST (urbancandy-db), se for local usa localhost
-    host: process.env.DB_HOST || 'localhost', 
+    host: process.env.DB_HOST || 'localhost',
     dialect: 'mysql',
     logging: false,
   }
@@ -17,9 +17,8 @@ export const dataBaseConectionn = async () => {
     await sequelize.authenticate();
     console.log('Conexão com o MySQL (Sequelize) estabelecida!');
 
-    await sequelize.sync({ alter: true }); 
+    await sequelize.sync({ alter: true });
     console.log('Tabelas sincronizadas com sucesso no banco do Docker!');
-    
   } catch (error) {
     console.error('Erro ao conectar ao banco:', error);
   }
